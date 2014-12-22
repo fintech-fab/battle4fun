968f5d1bfe3b1ecbd961f57522ab8800152f5d58
rkravchik <roman@kravchik.ru>
2014-12-22 17:51:10 +0300

    Anteater rule added (test)

---
+++ game/rules/anteater.js
@@ -0,0 +1,29 @@
+/**
+ * ПРАВИЛО: Муравьед
+ * С вероятностью chance муравей может быть съеден муравьедом.
+ * на текущей клетке игрового поля
+ */
+
+
+/**
+ * @return {Unit[]} юнит, которого съели
+ */
+module.exports = function (constructor) {
+
+	var key, unit, cells = {}, failsIds = [];
+
+	// Текущий юнит
+	var unit = constructor.Unit;
+	// base координаты базы
+	var base = constructor.MapModel.bases[constructor.Unit.Player.baseNumber()];
+
+	/** @namespace constructor.params.chance */
+	// chance - вероятность быть съеденым муравьедом
+	var chance = constructor.params.chance / 100;
+
+	if (Math.random() > (1 - chance))
+	    unit.emitDestroy();
+
+};
+
+
