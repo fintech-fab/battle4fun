8c4e230bfee9b22ecbfa680b2ac8113466e4f882
rkravchik <roman@kravchik.ru>
2014-12-26 18:14:35 +0300

    в socket.js добавлен 'list' для вывода viewer.js списка игр в базе в консоль.

--- html/socket.js
+++ html/socket.js
@@ -3,6 +3,7 @@ var io = require('socket.io').listen(8888);
 var fetchGame = require('../game/item.js');
 var fetchMap = require('../map/item.js');
 var fetchStates = require('../game/states.js');
+var gameModel = require('../database/game.js');
 
 io.sockets.on('connection', function (socket) {
 
@@ -10,6 +11,20 @@ io.sockets.on('connection', function (socket) {
 
 	// жмем браузеру руку
 	socket.emit('handshake');
+
+	socket.on('list', function() {
+						gameModel.find(function(err, gamesList) {
+											var list = [];
+											if (err) {
+
+											} else {
+												for (var i = 0; i < gamesList.length; i++)
+													list.push(gamesList[i].id)
+											};
+
+											socket.emit('list', list);
+						})
+	});
 
 	// ждем id игры
 	socket.on('game', function (gameId) {
